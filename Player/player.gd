7f4f1ba91e5f22afa5a25300db120659d3602ad7
juuj <jurajhadviger@gmail.com>
2025-07-11 22:54:18 +0200

    added sliding and coyote time and stuff, gonna work on levitating controller

--- Player/player.gd
+++ Player/player.gd
@@ -34,33 +34,70 @@ func shoot():
 
 
 
+
 const GRAV := 9.5
 const FALL_GRAV := 10.0
+const SKYDIVE_GRAV_BOOST := 15.0
 const JUMP_VELOCITY := 4.0
+
 const MAX_SPEED := 8.0
 var acceleration := 0.0
-const GROUND_ACCELERATION := 20.0
-const AIR_ACCELERATION := 1.0
-const GROUND_DECELERATION := 7.0
+const GROUND_ACCELERATION := 45.0
+const AIR_ACCELERATION := 33.0
+const GROUND_FRICTION := 45.0
 
 var input_dir := Vector2.ZERO
 var move_dir := Vector3.ZERO
 
 var sliding := false
+const SLIDE_ACCELERATION := 5.0
+const SLIDE_FRICTION := 5.0
+const SLIDE_BOOST := 1.2
+const SLIDE_LIMIT := 5.0
+
+var vel_buffer := []
+const VEL_BUFFER_SIZE := 4
 
 func _physics_process(delta):
+	slide()
+	
 	movement(delta)
 	
-	if is_on_floor() and not %JumpBuffer.is_stopped():
+	if is_on_floor(): %CoyoteTime.start()
+	
+	if not %CoyoteTime.is_stopped() and not %JumpBuffer.is_stopped():
+		%CoyoteTime.stop()
 		%JumpBuffer.stop()
 		velocity.y += JUMP_VELOCITY
 		AudioPlayer.play_audio("res://Assets/Audio/Jump.wav", null, Vector2(0.9, 1.1))
-		#%Jump.play()
+	
+	update_variables()
+
+func update_variables():
+	vel_buffer.push_front(velocity)
+	if vel_buffer.size() > VEL_BUFFER_SIZE:
+		vel_buffer.pop_back()
+
+func slide():
+	if Input.is_action_just_pressed("shift"): %SlideBuffer.start()
+	if not %SlideBuffer.is_stopped() and is_on_floor():
+		%SlideBuffer.stop()
+		%Sliding.play()
+		sliding = true
+		$CollisionShape3D.shape.height = 0.5
+		position.y -= 1
+		velocity = get_real_velocity() * SLIDE_BOOST
+	if (Input.is_action_just_released("shift") or not is_on_floor() or velocity.length() < SLIDE_LIMIT) and sliding:
+		%Sliding.stop()
+		sliding = false
+		$CollisionShape3D.shape.height = 2
+		position.y += 0.75
 
 func movement(delta):
 	if not is_on_floor():
 		var grav = GRAV
 		if velocity.y < 0: grav = FALL_GRAV
+		if Input.is_action_pressed("ctrl"): grav += SKYDIVE_GRAV_BOOST
 		velocity.y += -grav * delta
 		if global_position.y < -20: global_position = Vector3.UP*4; velocity.y = 0.0
 	
@@ -91,26 +128,21 @@ func movement(delta):
 	var add_speed = MAX_SPEED - current_speed
 	
 	if add_speed > 0:
-		var accel = 40 * delta
-		if is_on_floor(): accel = 0.9
+		var accel = AIR_ACCELERATION
+		if is_on_floor(): accel = GROUND_ACCELERATION
+		accel *= delta
 		accel = min(accel, add_speed)
 		velocity += wish_dir * accel
 	
 	if is_on_floor() and not sliding: # friction
-		velocity += (-hvel.normalized() + wish_dir) * 45.0 * min(hvel.length(), 1.0) * delta
+		velocity += (-hvel.normalized() + wish_dir) * GROUND_FRICTION * min(hvel.length(), 1.0) * delta
 	
 	
-	if Input.is_action_just_pressed("shift") and is_on_floor():
-		sliding = true
-		$CollisionShape3D.shape.height = 0.5
-		position.y -= 1
-	if Input.is_action_just_released("shift"):
-		sliding = false
-		$CollisionShape3D.shape.height = 2
-		position.y += 0.75
 	
-	if sliding:
-		print("slide")
+	
+	
+	#if sliding:
+		#print("slide")
 	
 	
 	move_and_slide()
